767e8c042cd6a482b3f4e9dd6bb161be4aaf35c1
suyash shrivastava <suyash2796@gmail.com>
2020-08-30 10:08:56 +0530

    adding sort csv module

---
+++ src/sort_csv.py
@@ -0,0 +1,29 @@
+import csv
+import os
+
+#use multiprocessing to sort large csv file in disk
+'''
+ToDo: implement below methods to handle csv files
+
+'''
+
+
+class CsvException(Exception):
+    pass
+
+class CsvHandler:
+
+    def __init__(self):
+        '''init method'''
+
+    def read_csv(self):
+        '''read csv file and craete reader object'''
+
+    def split_csv(self):
+        '''split csv into multiple csv's'''
+
+    def sort_csv(self):
+        '''sort csv'''
+
+    def merge_csv(self):
+        '''merge csv'''
